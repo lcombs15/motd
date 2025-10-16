b01f66acb6ec4c1faaa8d56a367e7ae504473d60
Lucas Combs <l-combs@hotmail.com>
2025-10-16 22:16:21 +0000

    fail2ban settings, cleanup

--- refresh.sh
+++ refresh.sh
@@ -5,8 +5,6 @@ MOTD=/etc/update-motd.d
 function link () {
     source=$1
     dest=$MOTD/$source
-    echo source $source
-    echo dest $dest
     sudo rm -f $dest
     sudo ln -sf $(pwd)/$source $dest
 }
@@ -15,13 +13,17 @@ function disable () {
     sudo chmod -x $MOTD/$1
 }
 
-
+echo Linking scripts to $MOTD
 link 10-hostname-color
 link 20-sysinfo
 link 20-uptime
 link 35-diskspace
+link 50-fail2ban
 link 60-docker
 
+echo Disabling system scripts in $MOTD
 disable 10-help-text
 disable 50-motd-news
 disable 50-landscape-sysinfo
+
+echo Done
